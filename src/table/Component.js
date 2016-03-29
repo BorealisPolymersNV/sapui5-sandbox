268e503a4171fa1af2bee33142d03406ed490646
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2016-03-29 15:31:33 +0200

    Restructured

--- src/table/Component.js
+++ src/table/Component.js
@@ -1,25 +1,34 @@
 sap.ui.define(['sap/ui/core/UIComponent'],
-	function (UIComponent) {
-		"use strict";
+  function (UIComponent) {
+    "use strict";
 
-		var Component = UIComponent.extend("sap.m.tutorial.walkthrough.05.Component", {
+    var Component = UIComponent.extend("wt.Component", {
 
-			metadata: {
-				config: {
-					sample: {
-						iframe: "index.html",
-						stretch: true,
-						files: [
-							"App.controller.js",
-							"App.view.xml",
-							"index.html"
+      metadata: {
+        rootView: "wt.view.App",
+        dependencies: {
+          libs: [
+              "sap.ui.table",
+              "sap.ui.unified",
+              "sap.m"
+				]
+        },
+
+        config: {
+          wt: {
+            iframe: "index.html",
+            stretch: true,
+            files: [
+                "App.controller.js",
+                "App.view.xml",
+                "index.html"
 						]
-					}
-				}
-			}
+          }
+        }
+      }
 
-		});
+    });
 
-		return Component;
+    return Component;
 
-	});
+  });
--- src/table/controller/App.controller.js
+++ src/table/controller/App.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("wt.App", {
+	return Controller.extend("wt.controller.App", {
 
 		onShowHello : function () {
 			/* eslint-disable no-alert */
