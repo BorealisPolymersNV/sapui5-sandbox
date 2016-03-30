5bb0feae3c24a350a30caf7fe49d197a7c13cc46
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2016-03-30 08:41:44 +0200

    Close the connection

--- src/mssql-test/index.js
+++ src/mssql-test/index.js
@@ -3,11 +3,12 @@ var sql = require('mssql');
 var log = console.log.bind(console, 'LOG');
 var error = console.log.bind(console, 'ERROR');
 
-sql.connect("mssql://SAPMII_ESLB-Sandbox2_RW:Mfb93(aQPS@sapmii_db_w.mignetwork.net/SAPMII_ESLB-Sandbox2")
-    .then(function() {
-    
-        new sql.Request().query('select * from EventDef')
-            .then(log)
-    .catch(error);
- 
-}).catch(error);
+var conn = new sql.connect("mssql://SAPMII_ESLB-Sandbox2_RW:Mfb93(aQPS@sapmii_db_w.mignetwork.net/SAPMII_ESLB-Sandbox2")
+  .then(function () {
+    return new sql.Request().query('select * from EventDef');
+  })
+  .then(function(res){
+    log(res);
+    return sql.close();
+  })
+  .catch(error);
